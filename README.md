4d4299d2e05ec1c190ede5a02bd996cf0e58d21c
= <=>
2015-09-18 19:37:29 +0700

    Initial OVR Support Achieved

---
+++ README.md
@@ -0,0 +1,14 @@
+# golang - vr - linux
+
+
+Linux implementation of OVR C API 0.5.1 in GoLang
+
+====== Ongoing ======
+
+_ Add golang gl support
+_ Launch VR window to Oculus DK2
+_ Add bluetooth input support
+
+===== COMPLETED =====
+
+X Connect to OVR SDK
---
+++ main.go
@@ -0,0 +1,22 @@
+
+package main
+//Let's try importing the ovr c library
+
+/*
+#cgo CFLAGS: -I/usr/local/include
+#cgo LDFLAGS: -L/usr/lib/ -L/usr/local/lib/ -lovr -lglfw3 -lX11 -lXxf86vm -lXrandr -lpthread -lrt -lGL -lm -lstdc++
+#include <OVR_CAPI.h>
+#include <OVR_CAPI_GL.h>
+*/
+import "C"
+
+
+import "fmt"
+
+func main() {
+
+	fmt.Printf("Importing worked.\n")
+
+	C.ovr_Initialize(nil)
+	fmt.Printf("Initialized\n")	
+}
